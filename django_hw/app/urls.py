c38eed0b1e9325dabd18c8aea0273f0f5c185aa5
DiAnonim <akopyan.2002@mail.ru>
2024-03-05 11:55:57 +0500

    регулярные выражения для маршрутов

--- django_hw/app/urls.py
+++ django_hw/app/urls.py
@@ -3,8 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    # path('', home, name='home'),
     path('', base, name='base'),
-    path('todo_list/', todo_list, name='todo_list'),
-    re_path(r'^todo_detail/(?P<task_id>\d+)/$', todo_detail, name='todo_detail'),
+    re_path(r'^todo_list/$', todo_list, name='todo_list'), # все задачи на странице
+    re_path(r'^todo_detail/(?P<task_id>\d+)/$', todo_detail, name='todo_detail'), # подробная информация по задаче
 ]
