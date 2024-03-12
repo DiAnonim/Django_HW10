cac4fa811bb9b10bef9643bffa656760433d9f1e
DiAnonim <akopyan.2002@mail.ru>
2024-03-12 15:15:55 +0500

    added controllers

--- django_hw/app/urls.py
+++ django_hw/app/urls.py
@@ -3,7 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('', base, name='base'),
-    re_path(r'^todo_list/$', todo_list, name='todo_list'), # все задачи на странице
-    re_path(r'^todo_detail/(?P<task_id>\d+)/$', todo_detail, name='todo_detail'), # подробная информация по задаче
+    path('', base, name='home'),
+    re_path(r'^todo_list/$', ListTodo.as_view(), name='todo_list'), # все задачи на странице
+    re_path(r'^todo_detail/(?P<pk>\d+)/$', TodoDetail.as_view(), name='todo_detail'), # подробная информация по задаче
+    re_path(r'^create_todo/$', CreateTodo.as_view(), name='create_todo'), # создание задачи
+    re_path(r'^update_todo/(?P<pk>\d+)/$', UpdateTodo.as_view(), name='update_todo'), # редактирование задачи
+    re_path(r'^delete_todo/(?P<pk>\d+)/$', DeleteTodo.as_view(), name='delete_todo'), # удаление задачи
 ]
--- django_hw/app/views.py
+++ django_hw/app/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Todo
+from app.forms import TodoForm
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 
 def base(request):
     return render(request, 'base.html')
 
-def todo_list(request):
-    todos = Todo.objects.exclude(status='D').values_list( "id" ,"title", "status", "deadline")
-    return render(request, 'todo_list.html', {'todos': todos})
+# def todo_list(request):
+#     todos = Todo.objects.exclude(status='D').values_list( "id" ,"title", "status", "deadline")
+#     return render(request, 'todo_list.html', {'todos': todos})
 
-def todo_detail(request, task_id):
-    todo = Todo.objects.get(pk=task_id)
-    return render(request, 'todo_detail.html', {'todo': todo})
+# def todo_detail(request, task_id):
+#     todo = Todo.objects.get(pk=task_id)
+#     return render(request, 'todo_detail.html', {'todo': todo})
+
+class ListTodo(ListView):
+    model = Todo
+    template_name = 'todo_list.html'
+    context_object_name = 'todos'
+
+class TodoDetail(DetailView):
+    model = Todo
+    template_name = 'todo_detail.html'
+    context_object_name = 'todo'
+
+class CreateTodo(CreateView):
+    model = Todo
+    form_class = TodoForm
+    template_name = 'create_todo.html'
+    success_url = reverse_lazy('home')
+
+class UpdateTodo(UpdateView):
+    model = Todo
+    form_class = TodoForm
+    template_name = 'update_todo.html'
+    success_url = reverse_lazy('home')
+
+class DeleteTodo(DeleteView):
+    model = Todo
+    template_name = 'delete_todo.html'
+    success_url = reverse_lazy('home')
+    
+
